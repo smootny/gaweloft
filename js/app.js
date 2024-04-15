b9d7e4353a03318da21dd9795b7e1d161fddef9c
smootny <blazejewski@post.pl>
2024-04-15 23:33:22 +0200

    added new styling for popups

---
+++ js/app.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;  // Assuming the first slide should be shown first
+
+  function showSlide(index) {
+    slides[currentSlide].removeAttribute('data-active');
+    currentSlide = (index + slides.length) % slides.length;
+    slides[currentSlide].setAttribute('data-active', 'true');
+  }
+
+  document.querySelector("[data-carousel-button='prev']").addEventListener("click", () => {
+    showSlide(currentSlide - 1);
+  });
+
+  document.querySelector("[data-carousel-button='next']").addEventListener("click", () => {
+    showSlide(currentSlide + 1);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const openBtn = document.getElementById('contactBtn');
+  const closeBtn = document.getElementById('closeBtn');
+  const popup = document.getElementById('popupForm');
+
+  openBtn.addEventListener('click', function() {
+    popup.style.display = 'block';
+  });
+
+  closeBtn.addEventListener('click', function() {
+    popup.style.display = 'none';
+  });
+});

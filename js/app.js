555fc6c970841f67492534068f2bd618b80216f6
smootny <blazejewski@post.pl>
2024-04-23 18:40:46 +0200

    added logic for gradient and styling

--- js/app.js
+++ js/app.js
@@ -1,84 +1,30 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   const slides = document.querySelectorAll(".slide");
-//   let currentSlide = 0;  // Assuming the first slide should be shown first
-//
-//   function showSlide(index) {
-//     slides[currentSlide].removeAttribute('data-active');
-//     currentSlide = (index + slides.length) % slides.length;
-//     slides[currentSlide].setAttribute('data-active', 'true');
-//   }
-//
-//   document.querySelector("[data-carousel-button='prev']").addEventListener("click", () => {
-//     showSlide(currentSlide - 1);
-//   });
-//
-//   document.querySelector("[data-carousel-button='next']").addEventListener("click", () => {
-//     showSlide(currentSlide + 1);
-//   });
-// });
-//
-// document.addEventListener("DOMContentLoaded", function() {
-//   const openBtn = document.getElementById('contactBtn');
-//   const closeBtn = document.getElementById('closeBtn');
-//   const popup = document.getElementById('popupForm');
-//
-//   openBtn.addEventListener('click', function() {
-//     popup.style.display = 'block';
-//   });
-//
-//   closeBtn.addEventListener('click', function() {
-//     popup.style.display = 'none';
-//   });
-// });
-//
-// document.addEventListener("DOMContentLoaded", function() {
-//   const mainContainer = document.querySelector('.kontenerek');
-//   const captions = document.querySelectorAll('.os-phrases h2');
-//
-//   function handleScrollAnimations() {
-//     // Detect if the scroll is past half of the first section
-//     const triggerHeight = window.innerHeight / 2;
-//     captions.forEach((caption, index) => {
-//       const fadeDirection = index % 2 === 0 ? 'fade-out-right' : 'fade-out-left';
-//       if (mainContainer.scrollTop > triggerHeight) {
-//         caption.classList.add(fadeDirection);
-//       } else {
-//         caption.classList.remove('fade-out-left', 'fade-out-right');
-//         caption.style.animation = ''; // Reset animation to allow it to be triggered again
-//       }
-//     });
-//   }
-//
-//   mainContainer.addEventListener('scroll', handleScrollAnimations);
-//
-//   // Reset animations on navigation or carousel control clicks
-//   document.querySelectorAll('nav a, .carousel-button').forEach(control => {
-//     control.addEventListener('click', () => {
-//       captions.forEach(caption => {
-//         caption.classList.remove('fade-out-left', 'fade-out-right');
-//         caption.style.animation = ''; // Reset animation to re-trigger it
-//         setTimeout(() => { // Timeout to ensure animation restarts
-//           handleScrollAnimations();
-//         }, 50);
-//       });
-//     });
-//   });
-// });
-
 document.addEventListener("DOMContentLoaded", function() {
   const slides = document.querySelectorAll(".slide");
   let currentSlide = 0;
   const mainContainer = document.querySelector('.kontenerek');
   const captions = document.querySelectorAll('.os-phrases h2');
   let animationTriggered = false; // Flag to check if animation has been triggered
-
+  const firstSlideGradient = document.querySelector('.gradient');
+let gradientHidden = false;
   function showSlide(index) {
     slides[currentSlide].removeAttribute('data-active');
     currentSlide = (index + slides.length) % slides.length;
     slides[currentSlide].setAttribute('data-active', 'true');
     if (!animationTriggered) {
-      triggerCaptionsAnimation();
+      triggerCaptionsAnimation()
+      hideGradient();
       animationTriggered = true; // Set the flag as true after first trigger
+    }
+  }
+
+    function hideGradient() {
+    if (!gradientHidden) {  // Check if the gradient has already been hidden
+      firstSlideGradient.style.opacity = '0';
+      firstSlideGradient.style.transition = 'opacity 1s ease-in-out';
+      firstSlideGradient.addEventListener('transitionend', () => {
+        firstSlideGradient.style.display = 'none';
+      });
+      gradientHidden = true;  // Set flag to true after hiding the gradient
     }
   }
 
@@ -95,17 +41,20 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   document.querySelector("[data-carousel-button='prev']").addEventListener("click", () => {
-    showSlide(currentSlide - 1);
+    showSlide(currentSlide - 1)
+    ;
   });
 
   document.querySelector("[data-carousel-button='next']").addEventListener("click", () => {
-    showSlide(currentSlide + 1);
+    showSlide(currentSlide + 1)
+    ;
   });
 
   document.querySelectorAll('nav a, .carousel-button').forEach(control => {
     control.addEventListener('click', () => {
       if (!animationTriggered) {
-        triggerCaptionsAnimation();
+        triggerCaptionsAnimation()
+        hideGradient();
       }
     });
   });
@@ -117,20 +66,19 @@ document.addEventListener("DOMContentLoaded", function() {
   openBtn.addEventListener('click', () => {
     popup.style.display = 'block';
     if (!animationTriggered) {
-      triggerCaptionsAnimation();
+      triggerCaptionsAnimation()
+      hideGradient();
     }
   });
 
   closeBtn.addEventListener('click', () => {
     popup.style.display = 'none';
-    if (!animationTriggered) {
-      triggerCaptionsAnimation();
-    }
   });
 
   mainContainer.addEventListener('scroll', () => {
     if (!animationTriggered) {
-      triggerCaptionsAnimation();
+      triggerCaptionsAnimation()
+      hideGradient();
     }
   });
 });
